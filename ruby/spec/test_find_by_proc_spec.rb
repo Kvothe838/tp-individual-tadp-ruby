14a15cc03e493ecc7a2d704549722d6642cc213c
Kvothe838 <facundoyuffrida@gmail.com>
2021-06-05 11:51:42 -0300

    Pasa tests individuales pero no juntos

--- ruby/spec/test_find_by_proc_spec.rb
+++ ruby/spec/test_find_by_proc_spec.rb
@@ -1,21 +1,45 @@
 require_relative 'spec_models/find_by_proc/student_find_by_proc.rb'
 
 describe 'test_find_by_proc' do
-  it 'retorna los estudiantes con grade mayor a 6' do
-    estudiante_aprobado = Student.new
-    nombre_estudiante_aprobado = "Pedro Perez"
-    estudiante_aprobado.full_name = nombre_estudiante_aprobado
-    estudiante_aprobado.grade = 7
-    estudiante_desaprobado = Student.new
-    nombre_estudiante_desaprobado = "Maria Sanchez"
-    estudiante_desaprobado.full_name = nombre_estudiante_desaprobado
-    estudiante_desaprobado.grade = 5
+  let(:nombre_un_estudiante) do
+    "Pedro Perez"
+  end
 
-    estudiante_aprobado.save!
-    estudiante_desaprobado.save!
+  let(:un_estudiante) do
+    un_estudiante = Student.new
+    un_estudiante.full_name = nombre_un_estudiante
+    un_estudiante.grade = 7
+    un_estudiante
+  end
+
+  let(:nombre_otro_estudiante) do
+    "Maria Sanchez"
+  end
+
+  let(:otro_estudiante) do
+    otro_estudiante = Student.new
+    otro_estudiante.full_name = nombre_otro_estudiante
+    otro_estudiante.grade = 5
+    otro_estudiante
+  end
+
+  # it 'retorna los estudiantes con id igual a 5' do
+  #   Student.find_by_id("5")
+  # end
+
+  it 'retorna los estudiantes con grade mayor a 6' do
+    un_estudiante.save!
+    otro_estudiante.save!
 
     estudiantes_aprobados = Student.find_by_grade(proc { |grade| grade > 6 })
     expect(estudiantes_aprobados.length).to be 1
-    expect(estudiantes_aprobados[0].full_name).to eq(nombre_estudiante_aprobado)
+    expect(estudiantes_aprobados[0].full_name).to eq(nombre_un_estudiante)
+  end
+
+  it 'falla porque el bloque lanza un error' do
+    un_estudiante.save!
+    otro_estudiante.save!
+
+    expect {Student.find_by_full_name(proc { |name| name.this_message_doesnt_exist()})}.to raise_error(NoMethodError)
   end
 end
